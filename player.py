b24c30d3ec1ef323abfe4ea0aa56109ace511d09
Bence Kiglics <bence.kiglics@gmail.com>
2016-12-03 10:10:57 +0100

    All in if we have a pair

--- player.py
+++ player.py
@@ -3,6 +3,13 @@ class Player:
     VERSION = "Default Python allin player"
 
     def betRequest(self, game_state):
+        in_action = game_state['in_action']
+        current_player = game_state['players'][in_action]
+        cards = current_player['hole_cards']
+
+        if cards[0]['rank'] == cards[1]['rank']:
+            return 1000
+
         return 0
 
     def showdown(self, game_state):
