f673add520319fdbdf2092811909dd6e8affb21d
szenti <mszentivanyi@emarsys.com>
2016-12-03 11:49:24 +0100

    Ranking helper: add two_pairs, is_drill

--- ranking_helper_test.py
+++ ranking_helper_test.py
@@ -3,11 +3,8 @@ import unittest
 from ranking_helper import RankingHelper
 
 
-class PlayerTest(unittest.TestCase):
-    # def setUp(self):
-    #     self.rankingHelper = RankingHelper()
-
-    def test_is_pair(self):
+class RankingHelperTest(unittest.TestCase):
+    def test_is_pair_GivenPair_ReturnsTrue(self):
         helper = RankingHelper([
             {"rank": 4},
             {"rank": 9},
@@ -15,6 +12,28 @@ class PlayerTest(unittest.TestCase):
         ])
 
         self.assertTrue(helper.is_pair())
+
+    def test_is_two_pairs_GivenTwoPairs_ReturnsTrue(self):
+        helper = RankingHelper([
+            {"rank": 4},
+            {"rank": 9},
+            {"rank": 4},
+            {"rank": 9},
+            {"rank": 8},
+        ])
+
+        self.assertTrue(helper.is_two_pairs())
+
+    def test_is_drill_GivenDrill_ReturnsTrue(self):
+        helper = RankingHelper([
+            {"rank": 2},
+            {"rank": 9},
+            {"rank": 2},
+            {"rank": 7},
+            {"rank": 2},
+        ])
+
+        self.assertTrue(helper.is_drill())
 
 
 if __name__ == '__main__':
