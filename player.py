4f3684c6ccbb52ffd417e1f2efab428f364dea3a
szenti <mszentivanyi@emarsys.com>
2016-12-03 10:33:06 +0100

    High pair fix

--- player.py
+++ player.py
@@ -8,14 +8,12 @@ class Player:
         current_player = game_state['players'][in_action]
         cards = current_player['hole_cards']
 
-        players = game_state['players']
 
-        #call_value = game_state['current_buy_in'] - players[game_state['in_action']] + game_state['minimum_raise']
-        #print "call value: {0}".format(call_value)
+        call_value = game_state['current_buy_in'] - current_player['bet'] + game_state['minimum_raise']
 
         if cards[0]['rank'] == cards[1]['rank']:
             if cards[0]['rank'] in ("Q", "K", "A"):
-                return 1000
+                return call_value
             return 1000
 
         return 0
