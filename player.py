317e1ae6bba3a1ccc79ec37788e4b8a3624c55cb
juditandrasi <knifeofdreams86@gmail.com>
2016-12-03 13:43:02 +0100

    revert

--- player.py
+++ player.py
@@ -12,7 +12,6 @@ log = logging.getLogger('player.Player')
 log.addHandler(logging.StreamHandler(sys.stderr))
 log.setLevel(logging.DEBUG)
 
-
 class Player:
     VERSION_FORMAT = "Cautious parrot [config: {config}]"
     VERSION = "Cautious parrot"
@@ -22,57 +21,26 @@ class Player:
         self.VERSION = self.VERSION_FORMAT.format(config=self.config.version)
 
     def betRequest(self, game_state):
-        bet = 0
         in_action = game_state['in_action']
         current_player = game_state['players'][in_action]
         hand_cards = current_player['hole_cards']
         all_cards = hand_cards + game_state['community_cards']
-        ranking_service_all_cards = RankingHelper(all_cards)
-        ranking_service_all_cards.computeFrequencies()
-        all_in_value = current_player["stack"]
-        minimum_raise = int(game_state["minimum_raise"])
-        small_blind = int(game_state["small_blind"])
-
-        is_preflop = len(game_state["community_cards"]) == 0
+        # helper = RankingHelper(all_cards)
 
         call_value = game_state['current_buy_in'] - current_player['bet'] + game_state['minimum_raise']
-        active_player_count = len(filter(lambda player: player["status"] == "active", game_state["players"]))
-        chen_ranking = ranking_service_all_cards.get_chen_ranking()
 
-        if is_preflop:
-            if active_player_count == 2:
-                if chen_ranking >= 4.5:
-                    bet = all_in_value
-            elif active_player_count == 3:
-                if chen_ranking >= 5.5:
-                    bet = all_in_value
-            elif active_player_count == 4:
-                if chen_ranking >= 6:
-                    bet = all_in_value
-
-            if bet != all_in_value:
-                did_somebody_raise = minimum_raise >= small_blind * 2
-
-                if not did_somebody_raise:
-                    bet = minimum_raise * 2
-
-            if minimum_raise > small_blind * 8 and chen_ranking >= 7.5:
-                bet = all_in_value
-        else:
-            if hand_cards[0]['rank'] == hand_cards[1]['rank']:
-                if hand_cards[0]['rank'] in ("Q", "K", "A"):
-                    bet = call_value + self.config.bet_on_high_pair
-                    log.info('decision betting: %d', bet)
-                    return bet
-
-                bet = self.config.bet_on_pair
+        if hand_cards[0]['rank'] == hand_cards[1]['rank']:
+            if hand_cards[0]['rank'] in ("Q", "K", "A"):
+                bet = call_value + self.config.bet_on_high_pair
                 log.info('decision betting: %d', bet)
                 return bet
 
-            log.info('decision fold')
-            return 0
+            bet = self.config.bet_on_pair
+            log.info('decision betting: %d', bet)
+            return bet
 
-        return bet
+        log.info('decision fold')
+        return 0
 
     def showdown(self, game_state):
         log.info("number_of_community_cards %d", len(game_state['community_cards']))
