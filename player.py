67e5a8d56e1bf97a4cd9720d403b32667f8fb491
juditandrasi <knifeofdreams86@gmail.com>
2016-12-03 15:39:11 +0100

    prepare for new post flop

--- player.py
+++ player.py
@@ -62,16 +62,20 @@ class Player:
             if minimum_raise > small_blind * 8 and chen_ranking >= self.config.high_raise_min_chen_ranking:
                 bet = min(call_value, all_in_value/2)
         else:
-            if hand_cards[0]['rank'] == hand_cards[1]['rank']:
-                if hand_cards[0]['rank'] in ("Q", "K", "A"):
-                    bet = call_value + self.config.bet_on_high_pair
-                    log.info('decision betting: %d', bet)
-                else:
-                    bet = self.config.bet_on_pair
-                    log.info('decision betting: %d', bet)
+            if self.config.post_flop:
+                # new post flop code here
+                pass
             else:
-                log.info('decision fold')
-                return 0
+                if hand_cards[0]['rank'] == hand_cards[1]['rank']:
+                    if hand_cards[0]['rank'] in ("Q", "K", "A"):
+                        bet = call_value + self.config.bet_on_high_pair
+                        log.info('decision betting: %d', bet)
+                    else:
+                        bet = self.config.bet_on_pair
+                        log.info('decision betting: %d', bet)
+                else:
+                    log.info('decision fold')
+                    return 0
 
         if self.config.fold_over_max_stack_ratio:
             if current_player['bet'] > all_in_value * self.config.max_stack_ratio:
