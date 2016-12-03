d50956f7e818fa76e1f1d7fcc9d9e704f7f85901
juditandrasi <knifeofdreams86@gmail.com>
2016-12-03 10:03:02 +0100

    test to return an int

---
+++ player_test.py
@@ -0,0 +1,17 @@
+import unittest
+import json
+
+from player import Player
+
+class PlayerTest(unittest.TestCase):
+    def setUp(self):
+        self.player = Player()
+
+    def test_bet_request_should_return_integer(self):
+        with open('GameState.json') as json_file:
+            game_state = json.load(json_file)
+        self.assertIsInstance(self.player.betRequest(game_state), int)
+
+
+if __name__ == '__main__':
+    unittest.main()
