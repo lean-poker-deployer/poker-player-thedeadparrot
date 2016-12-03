b1101a9f1e65bad862d87d7019103f5b41dcc57a
Bence Kiglics <bence.kiglics@gmail.com>
2016-12-03 11:43:24 +0100

    removed showdown log

--- player.py
+++ player.py
@@ -32,5 +32,4 @@ class Player:
         return 0
 
     def showdown(self, game_state):
-        log.info('showdown')
         log.info("number_of_community_cards %d", len(game_state['community_cards']))
